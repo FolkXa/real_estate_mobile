9fc48ee5eb158f29fb4e0b1e24084bb6b6927bd9
Folk <jirakit.c@terradigitalventures.com>
2025-03-22 19:51:05 +0700

    fix git ignore and login page

--- flutter-backend/src/entities/image_real_estate.entity.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    createTable: `CREATE TABLE IF NOT EXISTS image_real_estate (
-        id SERIAL PRIMARY KEY,
-        real_estate_id INT NOT NULL REFERENCES real_estates(id) ON DELETE CASCADE,
-        image_path TEXT NOT NULL
-    );`
-};
